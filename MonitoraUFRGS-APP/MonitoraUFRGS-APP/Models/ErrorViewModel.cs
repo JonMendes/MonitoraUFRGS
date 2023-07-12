ed3ac03cb198b148cad5837a4f9bca10918688cd
Jonathan Mendes <jonathangabrielmendes@gmail.com>
2023-07-12 12:29:15 -0300

    Criação dos arquivos .net/asp.net para o app web usando mvc

---
+++ MonitoraUFRGS-APP/MonitoraUFRGS-APP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MonitoraUFRGS_APP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
